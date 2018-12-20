6d290c98586bf5159e99980f6e9b2ab91ef6b382
KolinaE <46008170+KolinaE@users.noreply.github.com>
2018-12-20 16:08:52 +0200

    Add files via upload

--- src/hometask1/Start.java
+++ src/hometask1/Start.java
@@ -3,30 +3,33 @@ package hometask1;
 public class Start {
 
     public static void main(String[] args) {
+        System.out.println(" Results of operations for the integer data: ");
 
         int addResult = Calculator.add(5, 7);
-        System.out.println(" Our actual result of the addition is " + addResult);
+        System.out.println("Our actual result of the addition is " + addResult);
 
         int subResult = Calculator.sub(100, 35);
-        System.out.println(" Our actual result of the subtraction is " + subResult);
+        System.out.println("Our actual result of the subtraction is " + subResult);
 
         int divResult = Calculator.div(200, 50);
-        System.out.println(" Our actual result of the division is " + divResult);
+        System.out.println("Our actual result of the division is " + divResult);
 
         int mulResult = Calculator.mul(33, 3);
-        System.out.println(" Our actual result of the multiplication is " + mulResult);
+        System.out.println("Our actual result of the multiplication is " + mulResult);
+
+        System.out.println(" Results of operations for the float data: ");
 
         double doubleAddResult = Calculator.add(3D, 6.8);
-        System.out.println(" Our actual result of the addition is " + doubleAddResult);
+        System.out.println("Our actual result of the addition is " + doubleAddResult);
 
         double doubleSubResult = Calculator.sub(856D, 74.36);
-        System.out.println(" Our actual result of the subtraction is " + doubleSubResult);
+        System.out.println("Our actual result of the subtraction is " + doubleSubResult);
 
         double doubleDivResult = Calculator.div(17.3, 5D);
-        System.out.println(" Our actual result of the division is " + doubleDivResult);
+        System.out.println("Our actual result of the division is " + doubleDivResult);
 
         double doubleMulResult = Calculator.mul(44.03, 7D);
-        System.out.println(" Our actual result of the multiplication is " + doubleMulResult);
+        System.out.println("Our actual result of the multiplication is " + doubleMulResult);
     }
 }
 
